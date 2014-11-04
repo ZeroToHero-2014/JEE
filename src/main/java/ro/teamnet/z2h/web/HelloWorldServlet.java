6109a599b192b8d5166f20c41f4c6d38e2fd98f2
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2014-11-04 20:43:08 +0200

    first commit

---
+++ src/main/java/ro/teamnet/z2h/web/HelloWorldServlet.java
@@ -0,0 +1,33 @@
+/**
+ * HelloWorldServlet.java
+ *
+ * Copyright (c) 2014 Teamnet. All Rights Reserved.
+ *
+ * This source file may not be copied, modified or redistributed,
+ * in whole or in part, in any form or for any reason, without the express
+ * written consent of Teamnet.
+ **/
+
+package ro.teamnet.z2h.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Hello World Servlet expose get method to say hello
+ * to input user
+ */
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Hello <b>"+req.getParameter("user"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<div>
+    <form name="hello" method="post" action="${pageContext.request.contextPath}/hello">
+        <label for="user">User:</label>
+        <input type="text" id="user" name="user"/>
+    </form>
+</div>
+</body>
+</html>

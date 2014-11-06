e3abb9f7f87b224b4b98d3f2cd1b8c328409a863
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2014-11-06 22:19:33 +0200

    Hello World

---
+++ src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
@@ -0,0 +1,25 @@
+/**
+ * ZeroToHeroServlet.java
+ *
+ * Copyright (c) 2014 Teamnet. All Rights Reserved.
+ *
+ * This source file may not be copied, modified or redistributed,
+ * in whole or in part, in any form or for any reason, without the express
+ * written consent of Teamnet.
+ **/
+
+package ro.teamnet.z2h.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ZeroToHeroServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //todo
+    }
+}
